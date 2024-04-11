4038b2ce3a79c61cceae708b9d3abe5c26991775
Bongdarius <okuo94@gnu.ac.kr>
2024-04-12 00:54:55 +0900

    추가

--- python_workplace/2738.py
+++ python_workplace/2738.py
@@ -1,18 +1,38 @@
-# a = input().split(" ");
-# print(a);
-# a1 = int(a[0]);
-# a2 = int(a[1]);
-
-# arr1 = [];
-# arr2 = [];
-
-# print(arr1);
+numArr = input().split();
+num1 = int(numArr[0]);
+num2 = int(numArr[1]);
 
 a = [];
+b = [];
 
-for i in range(3):
-    a.append([]);
-    for j in range(3):
-        a[i].append(j);
+for i in range(num1):
+    arr = [int(x) for x in input().split()];
+    # arr = list(map(int, input().split()))
+    a.append(arr);
 
-print(a);
+for i in range(num1):
+    arr = [int(x) for x in input().split()];
+    # arr = list(map(int, input().split()))
+    b.append(arr);
+
+# for i in range(num1):
+    for j in range(num2):
+        a[i][j] += b[i][j];
+
+for i in range(num1):
+    mystr = "";
+    for j in range(num2):
+        mystr += str(a[i][j]) + " ";
+    print(mystr);
+
+# 행렬 덧셈
+# result = []
+# for i in range(num1):
+#     row = []
+#     for j in range(num2):
+#         row.append(a[i][j] + b[i][j])
+#     result.append(row)
+
+# # 결과 출력
+# for row in result:
+#     print(*row)

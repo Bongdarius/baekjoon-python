5d2cb1e6c3a3a39879aaccf29fe7862ba7ab2905
Bongdarius <okuo94@gnu.ac.kr>
2024-04-18 08:05:18 +0900

    해결 방법 찾음

--- python_workplace/CLASS_2/2839.py
+++ python_workplace/CLASS_2/2839.py
@@ -1,1 +1,12 @@
-weight = 18
+weight = int(input())
+# weight = 19
+list = []
+for i in range(weight // 5):
+    for j in range(weight // 3):
+        if(((i+1) * 5 + (j+1) * 3) == weight):
+            list.append([i+1, j+1])
+
+print(list)
+# listLength = len(list)
+# print(list[listLength-1])
+
